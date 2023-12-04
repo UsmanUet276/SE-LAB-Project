0cc90d7dec1b2d5c1092a6fce692286aa9618e4e
SumanShahzad123 <144985287+SumanShahzad123@users.noreply.github.com>
2023-12-04 21:57:14 +0500

    Updated Home Service

--- home_service_app/lib/main.dart
+++ home_service_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 //import 'splash_screen.dart';
 //import 'welcome_screen.dart';
 import 'login_screen.dart';
@@ -7,8 +7,8 @@ import 'login_screen.dart';
 //import 'sign_up.dart';
 
 void main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
