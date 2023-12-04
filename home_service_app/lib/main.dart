792f82bb8596454ddf9a4711f896ebaeb19e83c6
SumanShahzad123 <144985287+SumanShahzad123@users.noreply.github.com>
2023-12-04 22:15:04 +0500

    Updated

--- home_service_app/lib/main.dart
+++ home_service_app/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 //import 'splash_screen.dart';
-//import 'welcome_screen.dart';
+import 'welcome_screen.dart';
 import 'login_screen.dart';
 //import 'sign_in.dart';
 //import 'sign_up.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       //home: SplashScreen(),
-      //home: WelcomeScreen()
-      home: LoginScreen(),
+      home: WelcomeScreen()
+      //home: LoginScreen(),
       //home: SignIn()
       //home: SignUp()
     );
